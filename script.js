cd06c498217437d24b14919031efd928ff7bed93
yogniku <156093318+yogniku@users.noreply.github.com>
2024-07-13 15:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function chunkArray(myarry,chunksize) {
-	const result=[]
-	let n=6;
-	while (n < myarry.length) {
-		results.push(myarry.slice(index,index+chunksize))
-		index+=chunksize;
-	}
-	return results;
+function divide(arr, n) {
+    let result = [];
+    let currentSubarray = [];
+    let currentSum = 0;
+
+    for (let num of arr) {
+        if (currentSum + num <= n) {
+            currentSubarray.push(num);
+            currentSum += num;
+        } else {
+            result.push(currentSubarray);
+            currentSubarray = [num];
+            currentSum = num;
+        }
+    }
+
+    if (currentSubarray.length > 0) {
+        result.push(currentSubarray);
+    }
+
+    return result;
 }
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// Examples
+console.log(divide([1, 2, 3, 4, 1, 0, 2, 2], 5)); // Output: [[1, 2], [3], [4, 1, 0], [2, 2]]
+console.log(divide([4, 3, 2, 1], 4));            // Output: [[4], [3], [2], [1]]
+
